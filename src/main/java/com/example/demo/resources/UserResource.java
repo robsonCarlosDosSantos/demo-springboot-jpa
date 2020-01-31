403019d6c444ed65b03646097f1176222a2c437c
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-01-31 14:52:09 +0000

    JPA repository, dependency injection, database seeding

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,11 +1,11 @@
-package com.example.demo.resource;
+package com.example.demo.resources;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.User;
+import com.example.demo.entities.User;
 
 @RestController
 @RequestMapping("/users")
