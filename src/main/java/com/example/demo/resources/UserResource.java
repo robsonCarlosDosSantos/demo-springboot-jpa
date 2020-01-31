60341f99a41d1b6e9365f97d436bb2c80829ba25
Robson Carlos <rcs.robson.carlos.santos@gmail.com>
2020-01-31 15:35:29 +0000

    Service layer, component registration

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.example.demo.resources;
 
+import java.util.List;
+
+import javax.persistence.criteria.Path;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(23L,"Robson Carlos","robson@teste.com", "8888-8888","12345678");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = userService.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 
